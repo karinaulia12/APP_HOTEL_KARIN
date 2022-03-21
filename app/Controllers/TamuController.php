31b2e9a1b253b8e68e20348209e5d79c862743f0
Karin Aulia <96002478+karinaulia12@users.noreply.github.com>
2022-03-21 16:31:55 +0700

    menambahkan function checkin, checkout, simpanreservais, hapusdatareservasi, form, tolak, terima pada file TamuController

--- app/Controllers/TamuController.php
+++ app/Controllers/TamuController.php
@@ -39,14 +39,14 @@ class TamuController extends BaseController
 
         // query id_kamar di tabel reservasi_kamar
         $data_kamar = [];
-        if (count($reservasi) > 0)
+        if (count($reservasi) > 0) {
             $data_kamar = \config\Database::connect()
                 ->table('reservasi_kamar')
                 ->select('id_reservasi_kamar, no_kamar')
                 ->whereIn('id_reservasi', $id_reservasi)
                 ->join('kamar', 'kamar.id_kamar = reservasi_kamar.id_kamar')
                 ->get()->getResultArray();
-
+        }
         // $data_kamar = data mentah dari db
         // $data_kamar = data yang sudah distruktur ulang pengelompokannya
         // struktur ulang data_kamar
@@ -56,11 +56,10 @@ class TamuController extends BaseController
         }
 
         // memasukkan $data_kamar ke $reservasi
-        // $reservasi = array_map(
-        //     function ($reserv) use ($data_kamar_) {
+        // $reservasi = array_map(function ($reserv) use ($data_kamar_) {
         //     $reserv['kamar'] = [];
         //     if(array_key_exists($reserv['id_reservasi'], $data_kamar_)) {
-        //         $reserv['kamar'] = $data_kamar_[$reserv['id_reservasi']];
+        //         $reserv['kamar'] = $data_kamar_[$reserv['id_reservasi_kamar']];
         //     }
 
         //     switch ($reserv['status']) {
@@ -72,5 +71,100 @@ class TamuController extends BaseController
         //                 break;
         //     }
         // }
+    }
+
+    public function checkin($id_reservasi)
+    {
+        $berhasil = $this->reservasi->update($id_reservasi, ['status' => 2]);
+        return redirect()->to('/resepsionis/reservasi');
+    }
+
+    public function checkout($id_reservasi)
+    {
+        $berhasil = $this->reservasi->update($id_reservasi, ['status' => 3]);
+        return redirect()->to('/resepsionis/reservasi');
+    }
+
+    public function terima($id_reservasi)
+    {
+        $berhasil = $this->reservasi->update($id_reservasi, ['status' => 4]);
+        return redirect()->to('/resepsionis/reservasi');
+    }
+
+    public function tolak($id_reservasi)
+    {
+        $berhasil = $this->reservasi->update($id_reservasi, ['status' => 5]);
+        return redirect()->to('/resepsionis/reservasi');
+    }
+
+    public function hapusdatareservasi($id_reservasi)
+    {
+        $syarat = ['id_reservasi' => $id_reservasi];
+        $infoFile = $this->reservasiModel->where($syarat)->find();
+
+        $this->reservasiModel->where('id_reservasi', $id_reservasi)->delete();
+        return redirect()->to('/resepsionis/reservasi');
+    }
+
+    public function form()
+    {
+        // mengambil semua data kamar
+        $data = [
+            'kamar' => $this->kamarModel->findAll()
+        ];
+        return view('tamu/form-booking', $data);
+    }
+
+    public function simpan()
+    {
+        d($this->request->getPost());
+
+        $jml_kamar = count($this->request->getPost('pilihKamar'));
+        $id_kamarkamar = $this->request->getPost('pilihKamar');
+
+        // ambil harga kamar berd. tipe kamar
+        $this->kamarModel->select('harga')
+            ->whereIn('id_kamar', $id_kamarkamar)
+            ->join('type_kamar', 'type_kamar.id_type_kamar = kamar.id_type_kamar')
+            ->get('')->getResultArray();
+
+        $harga_kamar = 0;
+        foreach ($this->kamarModel as $value) {
+            $harga_kamar = $harga_kamar + $value['harga'];
+        }
+
+        $checkin = $this->request->getPost('checkin');
+        $checkout = $this->request->getPost('checkout');
+
+        $in = new \DateTime($checkin);
+        $out = new \DateTime($checkout);
+        $interval = $in->diff($out);
+        $jml_hari = $interval->d;
+
+        $this->reservasiModel->insert(
+            [
+                'nik' => $this->request->getPost('nik'),
+                'checkin' => $checkin,
+                'checkout' => $checkout,
+                'jml_kamar' => $jml_kamar,
+                'harga' => $harga_kamar,
+                'total' => $harga_kamar * $jml_hari,
+                'status' => 1
+            ]
+        );
+
+        $data = [];
+        foreach ($id_kamarkamar as $key => $value) {
+            $data[] = [
+                'id_kamar' => $value,
+                'id_reservasi' => $this->reservasiModel->getInsertId()
+            ];
+        }
+
+        $db = \config\Database::connect();
+        $builder = $db->table('reservasi_kamar');
+        $builder->insertBatch($data);
+
+        return redirect()->to('/reservasi/data');
     }
 }
